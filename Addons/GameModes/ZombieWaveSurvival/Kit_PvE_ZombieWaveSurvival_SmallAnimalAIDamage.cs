50575ae59bf275760b17dda70e1812bba1bf4f8d
Joshtapus <3kwikigames@gmail.com>
2022-06-24 16:02:05 -0500

    Small animals implemented.

---
+++ Addons/GameModes/ZombieWaveSurvival/Kit_PvE_ZombieWaveSurvival_SmallAnimalAIDamage.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+namespace MarsFPSKit
+{
+    namespace ZombieWaveSurvival
+    {
+        public class Kit_PvE_ZombieWaveSurvival_SmallAnimalAIDamage : MonoBehaviour, IKitDamageable
+        {
+            /// <summary>
+            /// The AI this one belongs to
+            /// </summary>
+            public Kit_PvE_ZombieWaveSurvival_SmallAnimalAI ai;
+            /// <summary>
+            /// Damage multiplier
+            /// </summary>
+            public float multiplier = 1f;
+            /// <summary>
+            /// Damage to apply
+            /// </summary>
+            public float damage = 20f;
+            /// <summary>
+            /// Which death sound to play
+            /// </summary>
+            public int deathSoundCategory;
+            
+            void Start()
+            {
+                if (!ai)
+                {
+                    ai = GetComponentInChildren<Kit_PvE_ZombieWaveSurvival_SmallAnimalAI>();
+                }
+            }
+
+            public bool LocalDamage(float dmg, int gunID, Vector3 shotPos, Vector3 forward, float force, Vector3 hitPos, bool shotBot, int shotId)
+            {
+                if (ai)
+                {
+                    //Give us money
+                    Kit_PvE_ZombieWaveSurvival_SmallAnimalAI.zws.localPlayerData.GainMoney(Kit_PvE_ZombieWaveSurvival_SmallAnimalAI.zws.moneyPerHit);
+                    //Relay to ai
+                    ai.LocalDamage(dmg, gunID);
+
+                    //Everyone can shoot these
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            private void OnTriggerEnter(Collider other)
+            {
+                Kit_PvE_ZombieWaveSurvival_ZombieDamageable damageable = other.GetComponentInParent<Kit_PvE_ZombieWaveSurvival_ZombieDamageable>();
+
+                if (damageable != null)
+                {
+                    if (damageable.IsAlive())
+                    {
+                        damageable.ApplyDamage(damage);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Addons/GameModes/ZombieWaveSurvival/Scripts/Kit_PvE_ZombieWaveSurvival_SmallAnimalAI.cs
@@ -0,0 +1,81 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MarsFPSKit
+{
+    namespace ZombieWaveSurvival
+    {
+        public class Kit_PvE_ZombieWaveSurvival_SmallAnimalAI :  MonoBehaviourPunCallbacks, IPunObservable
+        {
+            public GameObject waypoint1;
+            public GameObject waypoint2;
+            public float distanceToPlayer = 10f;
+            public float speed = .5f;
+            /// <summary>
+            /// How many hit points we have left
+            /// </summary>
+            private float hitPoints = 25f;
+            private float originalHP = 25f;
+            /// <summary>
+            /// Reference to main
+            /// </summary>
+            private Kit_IngameMain main;
+            /// <summary>
+            /// Cached reference to the game mode
+            /// </summary>
+            public static Kit_PvE_ZombieWaveSurvival zws;
+            
+            void Start() {
+                //Find main
+                main = FindObjectOfType<Kit_IngameMain>();
+                zws = main.currentPvEGameModeBehaviour as Kit_PvE_ZombieWaveSurvival;
+            }
+
+            void Update() {
+                float distance = Vector3.Distance (waypoint1.transform.position, transform.position);
+                if (distance <= distanceToPlayer) {
+                    Vector3 targetPosition = new Vector3(waypoint1.transform.position.x, waypoint1.transform.position.y - 2f, waypoint1.transform.position.z);
+                    transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                }
+            }
+
+            public void OriginalHealth()
+            {
+                if (photonView.IsMine)
+                {
+                    hitPoints = originalHP;
+                }
+            }
+
+            public void LocalDamage(float dmg, int gunID)
+            {
+                //Send RPC
+                photonView.RPC("RpcDamage", RpcTarget.MasterClient, dmg, gunID);
+            }
+
+            [PunRPC]
+            public void RpcDamage(float dmg, int gunID)
+            {
+                if (photonView.IsMine)
+                {
+                    //Apply damage
+                    hitPoints -= dmg;
+
+                    if (hitPoints <= 0)
+                    {
+                        //Send Event to give us kill xp
+                        // PhotonNetwork.RaiseEvent(101, null, new RaiseEventOptions { TargetActors = new int[] { shotId }, Receivers = ReceiverGroup.All }, new ExitGames.Client.Photon.SendOptions { Reliability = true });
+
+                        //Kill us
+                        PhotonNetwork.Destroy(gameObject);
+                    }
+                }
+            }
+
+            void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) { }
+        }
+    }
+}
